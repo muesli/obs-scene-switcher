4754df8fb79f5b1349824942ed0a15066fa59d6b
Christian Muehlhaeuser <muesli@gmail.com>
2020-02-05 03:35:03 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,46 @@
+# obs-scene-switcher
+
+[![GoDoc](https://godoc.org/github.com/golang/gddo?status.svg)](https://godoc.org/github.com/muesli/obs-scene-switcher)
+[![Go ReportCard](http://goreportcard.com/badge/muesli/obs-scene-switcher)](http://goreportcard.com/report/muesli/obs-scene-switcher)
+
+obs-scene-switcher is a command-line remote control for OBS
+
+## Installation
+
+Make sure you have a working Go environment (Go 1.11 or higher is required).
+See the [install instructions](http://golang.org/doc/install.html).
+
+To install obs-scene-switcher, simply run:
+
+    go get github.com/muesli/obs-scene-switcher
+
+## Configuration
+
+Edit scenes.toml and define which scenes you want to be connected to which
+windows, e.g.:
+
+```
+[[scenes]]
+    scene_name = "IDE"
+    window_class = "code-oss"
+
+[[scenes]]
+    scene_name = "Terminal"
+    window_name = "Konsole"
+
+[[scenes]]
+    scene_name = "Browser"
+    window_class = "Chromium"
+```
+
+In plain english, this means that whenever you focus your `VS Code` window, OBS
+will be asked to switch to the scene called `IDE`. If you focus your `Konsole`
+window it switches to the scene `Terminal`, and so on.
+
+## Usage
+
+Start obs-scene-switcher:
+
+```bash
+obs-scene-switcher -config scenes.toml
+```
---
+++ main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	obsws "github.com/christopher-dG/go-obs-websocket"
+	"github.com/spf13/cobra"
+)
+
+var (
+	configFile string
+	host       string
+	port       uint32
+
+	config Config
+
+	rootCmd = &cobra.Command{
+		Use:   "obs-scene-switcher",
+		Short: "obs-scene-switcher tracks your active window and switches scenes accordingly",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return execute()
+		},
+	}
+
+	client        *obsws.Client
+	recentWindows []Window
+)
+
+type Scene struct {
+	SceneName   string `toml:"scene_name"`
+	WindowClass string `toml:"window_class"`
+	WindowName  string `toml:"window_name"`
+}
+
+type Scenes []Scene
+
+type Config struct {
+	Scenes Scenes `toml:"scenes"`
+}
+
+func LoadConfig(filename string) (Config, error) {
+	config := Config{}
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return config, err
+	}
+
+	_, err = toml.Decode(string(b), &config)
+	return config, err
+}
+
+func main() {
+	var err error
+	config, err = LoadConfig(configFile)
+	if err != nil {
+		fmt.Println("could not load config file:", configFile)
+		os.Exit(1)
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if client != nil {
+		client.Disconnect()
+	}
+}
+
+func init() {
+	cobra.OnInitialize(connectOBS)
+	rootCmd.PersistentFlags().StringVar(&configFile, "config", "scenes.toml", "path to config file")
+	rootCmd.PersistentFlags().StringVar(&host, "host", "localhost", "host to connect to")
+	rootCmd.PersistentFlags().Uint32VarP(&port, "port", "p", 4444, "port to connect to")
+}
+
+func handleActiveWindowChanged(event ActiveWindowChangedEvent) {
+	fmt.Println(fmt.Sprintf("Active window changed to %s (%d, %s)",
+		event.Window.Class, event.Window.ID, event.Window.Name))
+
+	// remove dupes
+	i := 0
+	for _, rw := range recentWindows {
+		if rw.ID == event.Window.ID {
+			continue
+		}
+
+		recentWindows[i] = rw
+		i++
+	}
+	recentWindows = recentWindows[:i]
+
+	recentWindows = append([]Window{event.Window}, recentWindows...)
+	if len(recentWindows) > 15 {
+		recentWindows = recentWindows[0:15]
+	}
+
+	for _, v := range config.Scenes {
+		if event.Window.Class == v.WindowClass ||
+			event.Window.Name == v.WindowName {
+			req := obsws.NewSetCurrentSceneRequest(v.SceneName)
+			req.Send(*client)
+		}
+	}
+}
+
+func handleWindowClosed(event WindowClosedEvent) {
+	i := 0
+	for _, rw := range recentWindows {
+		if rw.ID == event.Window.ID {
+			continue
+		}
+
+		recentWindows[i] = rw
+		i++
+	}
+	recentWindows = recentWindows[:i]
+}
+
+func execute() error {
+	x := Connect(os.Getenv("DISPLAY"))
+	defer x.Close()
+
+	tch := make(chan interface{})
+	x.TrackWindows(tch, time.Second)
+
+	for e := range tch {
+		switch event := e.(type) {
+		case ActiveWindowChangedEvent:
+			handleActiveWindowChanged(event)
+
+		case WindowClosedEvent:
+			handleWindowClosed(event)
+		}
+	}
+
+	return nil
+}
+
+func connectOBS() {
+	// disable obsws logging
+	obsws.Logger = log.New(ioutil.Discard, "", log.LstdFlags)
+
+	client = &obsws.Client{Host: host, Port: int(port)}
+	if err := client.Connect(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Set the amount of time we can wait for a response.
+	obsws.SetReceiveTimeout(time.Second * 2)
+}
